aa2acee1051a5c91b21e87ccb172dcc0a1a7416e
abhishhhek <142973088+abhishhhek@users.noreply.github.com>
2024-12-27 08:54:17 +0000

    array end

--- basics02/arrays.js
+++ basics02/arrays.js
@@ -24,16 +24,40 @@ const newArr = myArr2.join(); //makes the array of string type and join all the 
 
 //slice and splice
 
-console.log("A ", myArr2);
+// console.log("A ", myArr2);
 
 const myn1 = myArr2.slice(1,3); //prints element at index 1 and 2
 
-console.log(myn1);
-console.log("B ",myArr2)
+// console.log(myn1);
+// console.log("B ",myArr2)
 
 //splice
  
 const myn2  = myArr2.splice(1,3)
-console.log("Cf ",myArr2)
-console.log(myn2);
+// console.log("Cf ",myArr2)
+// console.log(myn2);
+
+const marvel_heros = ["ironman","hulk","dr strange"]
+
+const dc_heros = ["batman", "superman","flash"]
+
+const All_heros = marvel_heros.concat(dc_heros);
+
+// console.log(All_heros);
+
+const All_new_heros = [...marvel_heros,...dc_heros]
+// console.log(All_new_heros);
+
+const arr = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+const arr2 = arr.flat(Infinity);
+
+console.log(arr2);
+
+
+console.log(Array.isArray("abhishek"));
+console.log(Array.from("abhishek"));
+console.log(Array.from({name: "abhishek"})); //interesting case
+
+
+
 
