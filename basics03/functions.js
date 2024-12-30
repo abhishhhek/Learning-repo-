d816744ceb6d25cd454622f611bf8706ca7ebeab
abhishhhek <142973088+abhishhhek@users.noreply.github.com>
2024-12-30 07:11:39 +0000

    function part 2  done

--- basics03/functions.js
+++ basics03/functions.js
@@ -31,4 +31,37 @@ function logInUserMessage(username = "default"){ //gives default value
     }
     return `${username} just logged in`
 }
-console.log(logInUserMessage());
+// console.log(logInUserMessage());
+
+
+
+function calculateCartPrice(val1,val2,...num1){ // ... is rest operator
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,400,500,2000))
+
+const user = {
+    username: "abhishek",
+    prices: "1999"
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.prices}`)
+}
+
+// handleObject(user);
+
+// handleObject({
+//     username:"abhishek",
+//     prices:199
+// })
+
+const myArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myArray));
+
+console.log(returnSecondValue([200,500,700]));
